d98be2c9ac6204664e5d15135170fe5be64df02b
cyntler <damian@cyntler.com>
2021-09-21 17:14:13 +0200

    Add HomePage

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Dc</title>
+      </Head>
+      <div>homepage</div>
+    </>
+  );
+};
+
+export default HomePage;
