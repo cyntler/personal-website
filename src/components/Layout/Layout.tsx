f7788d1563dd02a3b755a54b3bf1a8622fc83580
Damian Cyntler <damian@cyntler.com>
2021-11-29 18:25:21 +0100

    Fix Header path

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from 'react';
 
-import { Header } from '~/Header/Header';
+import { Header } from '~/components/Header/Header';
 import { Container, Content } from './Layout.style';
 
 export const Layout: FunctionComponent = ({ children }) => (
