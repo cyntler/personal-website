3d03e54745deb507f8abb872415685191036f416
Damian Cyntler <damian@cyntler.com>
2021-12-03 22:32:58 +0100

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   moduleNameMapper: {
     '^~(.*)$': '<rootDir>/src/$1',
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
