c1061d9a47f27416744061d636d4f3c7db7138b2
Damian Cyntler <damian@cyntler.com>
2021-12-08 18:47:04 +0100

    Add localeMessages prop to Providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,16 +1,24 @@
 import { FunctionComponent } from 'react';
 import { IntlProvider } from 'react-intl';
 
-import { useLocale } from '~/hooks/useLocale';
+import { LocaleMessages, useLocale } from '~/hooks/useLocale';
 
-export const Providers: FunctionComponent = ({ children }) => {
-  const { locale, defaultLocale, messages } = useLocale();
+interface ProvidersProps {
+  localeMessages: LocaleMessages;
+}
+
+export const Providers: FunctionComponent<ProvidersProps> = ({
+  children,
+  localeMessages,
+}) => {
+  const { locale, defaultLocale, messages } = useLocale(localeMessages);
 
   return (
     <IntlProvider
       locale={locale}
       defaultLocale={defaultLocale}
       messages={messages}
+      onError={() => null}
     >
       {children}
     </IntlProvider>
