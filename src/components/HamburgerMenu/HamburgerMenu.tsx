8050a646ca28ad0753071ba213f8691627e7b067
Damian Cyntler <damian@cyntler.com>
2022-01-10 19:59:08 +0100

    Update first mdx post

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from 'react';
-
-import { HamburgerMenuButton, HamburgerMenuIcon } from './HamburgerMenu.style';
-
-export const HamburgerMenu: FunctionComponent = () => (
-  <HamburgerMenuButton>
-    <HamburgerMenuIcon />
-  </HamburgerMenuButton>
-);
