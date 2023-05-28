722d71641216530676bbaa703578c5c17878b14b
Damian Cyntler <damian@cyntler.com>
2023-05-28 12:54:52 +0200

    chire: migrate from babel to swc

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const { join } = require('path');
 const { withLocalesConfig } = require('next-locales/server');
 
 module.exports = withLocalesConfig({
   reactStrictMode: true,
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: join(__dirname, '/node_modules/react'),
-      'react-dom': join(__dirname, '/node_modules/react-dom'),
-    };
-  },
+  swcMinify: true,
 });
