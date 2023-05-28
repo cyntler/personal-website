659a72d574f8313ea39e9fb1b07a060c1d9a572c
Damian Cyntler <damian@cyntler.com>
2023-05-28 12:54:38 +0200

    feat: install tailwind

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+    };
+  },
+}));
+
+jest.mock('next-locales', () => ({
+  useTranslation() {
+    return { t: (key: string) => key };
+  },
+}));
